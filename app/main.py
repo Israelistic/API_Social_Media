d932c1476d10325118de590f388ac2175f3b09fe
Haggai Lerman <haguyle@gmail.com>
2023-02-21 16:40:26 -0500

    Added new methods and routes
    Added route lastest and post{id}
    
    - the latest route will show the lastest posts
    - the route with id will show post by id and incorporate a status code
      404 if the ID not found

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import fastapi
 from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
@@ -17,24 +18,24 @@ class Post(BaseModel):
 # a global variable to store the dictionary posts
 my_posts = [{'title': 'title of a post 1', 'content': 'content of post 1', 'id': 1}, {'title': 'Favorite food', 'content': 'I like Schnizel', 'id': 2}]
 
-def find_posts(id):
+def find_post(id):
     for post in my_posts:
         if post['id'] == id:
             return post
 
-def find_index_posts(id):
+def find_index_post(id):
     for index, post in enumerate(my_posts):
         if post['id'] == id:
             return index
 
 ########### PATH OPERATION / ROUTE ###########
 
-
+# root site
 @app.get("/")
 def root():
     return {"message": "Welcome to my API!!"}
 
-
+# get the posts
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
@@ -46,3 +47,18 @@ def create_post(post: Post):
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+# The order of the routes matter. If the lastest path was under the post{id}-rotute it will be confuesd by a different route
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return {"detail": post}
+
+# The id in the route is path parameter
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id) # Since ID is str I converted it to interger so the find_post functuin can handle it.
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} was not found")
+    return {"post_detail": post}
+
