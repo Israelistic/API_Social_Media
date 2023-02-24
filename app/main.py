4a318ef5d1b4cc6a20f9f594d84abcc00f2872d1
Haggai Lerman <haguyle@gmail.com>
2023-02-23 21:45:54 -0500

    Feature: Added a DB
    I am now savig post into postgress db.
    The database will sit on my local host
    I have adjusted all path methods to fuction the crud operation from

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+# from psycopg2.extras import RealDictCursor
 
 
 app = FastAPI()
@@ -13,7 +16,21 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True # Optional for user to specify in the post request and it will default to true if not speccified
-    rating: Optional[int] = None # full optional field
+
+# connection to the database.
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='password123', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        # with psycopg.connect("host=localhost dbname=fastapi user=postgres") as conn:
+        #     with conn.cursor() as cur:       
+        print("Database connection was succesfull!")       
+        break
+    except Exception as error:
+        print("connection to database failed")
+        print("Error:", error)
+        time.sleep(2)
+        
 
 # a global variable to store the dictionary posts
 my_posts = [{'title': 'title of a post 1', 'content': 'content of post 1', 'id': 1}, {'title': 'Favorite food', 'content': 'I like Schnizel', 'id': 2}]
@@ -35,18 +52,20 @@ def find_index_post(id):
 def root():
     return {"message": "Welcome to my API!!"}
 
-# get the posts
+# get the posts from the database
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
-# create a post. print(post.dict() # sending back a dictionary which is a feature supported by pydantic
+# create a post. # and commit the changes to the database.
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts(title, content, published) VALUES(%s, %s, %s) RETURNING *""", (post.title, post.content, post.published)) # the %s prevents sql injection
+    new_post = cursor.fetchone() 
+    conn.commit() # commit the changes to the database
+    return {"data": new_post}
 
 # The order of the routes matter. If the lastest path was under the post{id}-rotute it will be confuesd by a different route
 @app.get("/posts/latest")
@@ -57,7 +76,8 @@ def get_latest_post():
 # The id in the route is path parameter
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id) # Since ID is str I converted it to interger so the find_post functuin can handle it.
+    cursor.execute("""SELECT * FROM posts WHERE id= %s """, (str(id)))
+    post = cursor.fetchone()               
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} was not found")
     return {"post_detail": post}
@@ -65,24 +85,20 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    # Deleting post
-    # Find the index in the array that has required ID
-    # my_post.pop(index)
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""", (str(id)))
+    deleted_post = cursor.fetchone() # saved the deleted post
+    conn.commit() # commit the changes to the database
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
-    my_posts.pop(index)
+    
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-
-    if index == None:
+    cursor.execute("""UPDATE posts SET title= %s, content= %s, published = %s WHERE id = %s RETURNING*""", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit() # commit the changes to the database
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
-    
-    post_dict = post.to_dict() # Convert to dictionary
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {'data': post_dict}
+    return {'data': updated_post}
